aaf84eb88eb82f9f89ec491c9489f89fee789390
George Fiotakis <g.fiotakis@hotmail.com>
2020-03-18 21:06:31 +0100

    Change gitgnor

---
+++ pre-deploy/mariadb/setup.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE sessionDB;
+
+USE sessionDB;
+
+CREATE TABLE `session_info` (
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token_id` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `access_token` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `expiring` int(11) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
