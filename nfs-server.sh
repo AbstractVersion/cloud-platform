d712d4cac20dba76f970a119a05af39769e21e41
George Fiotakis <g.fiotakis@hotmail.com>
2020-04-12 15:40:26 +0200

    update

--- nfs-server.sh
+++ nfs-server.sh
@@ -1,6 +1,7 @@
+#COnfiguration of server
 # In order to mount NFS drives we need to install the following:
 sudo -u root apt install nfs-common -y
-
+# https://sysadmins.co.za/docker-swarm-persistent-storage-with-nfs/
 # Create NFS Server:
 # We have 2 options, we can either install a NFS Server (you can follow this post to setup a NFS Server )  
 # or we can setup a NFS Server using Docker (explained below):
@@ -8,7 +9,7 @@ sudo -u root apt install nfs-common -y
 # Prepare the NFS Home Directory:
 mkdir /nfsdata
 
-
+## Change the ip to the ip of the NFS server host
 docker run --rm -itd --name nfs \
   --privileged \
   -v /nfsdata:/nfs.1 \
@@ -16,8 +17,46 @@ docker run --rm -itd --name nfs \
   -p 192.168.2.4:2049:2049 \
   itsthenetwork/nfs-server-alpine:latest
 
-#   Install Netshare Docker Volume Driver
+#   Configuration of Clients
+sudo -u root apt install nfs-common -y
+
+# Install theese external tool for volume share among managers
+# Install Netshare Docker Volume Driver
 # Install Netshare which will provide the NFS Docker Volume Driver:
 $ wget https://github.com/ContainX/docker-volume-netshare/releases/download/v0.36/docker-volume-netshare_0.36_amd64.deb
 $ dpkg -i docker-volume-netshare_0.36_amd64.deb
 $ service docker-volume-netshare start
+
+# docker create volume
+docker volume create --driver nfs --name foobar2 -o share=192.168.2.4:/foobar2
+docker volume inspect foobar2
+
+# Run a container with the volume namespace:
+docker run -i -t -v foobar2:/mount alpine /bin/sh
+
+## ON SERVER
+mkdir /nginx_web
+
+version: "3.7"
+services:
+  web:
+    image: nginx
+    volumes:
+      - nginx.vol:/usr/share/nginx/html
+    ports:
+      - 80:80
+    networks:
+      - web
+
+networks:
+  web:
+    driver: overlay
+    name: web
+
+volumes:
+  nginx.vol:
+    driver: nfs
+    driver_opts:
+      share: 10.0.2.15:/nginx_web
+
+      docker stack deploy -c docker-compose.yml app
--- test.sh
+++ test.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # This is a comment!
 
-# for NODE in $(docker node ls --filter role=manager --format '{{.Hostname}} ')
-# do 
-#     echo  "Adding as client :\t${NODE} - $(docker node inspect --format '{{.Status.Addr}}' "${NODE}")"; 
+ for NODE in $(docker node ls  --format '{{.Hostname}} ')
+ do 
+     echo  "Adding as client :\t${NODE} - $(docker node inspect --format '{{.Status.Addr}}' "${NODE}")"; 
 #     sudo -u root echo  "/mnt/sharedfolder $(docker node inspect --format '{{.Status.Addr}}' "${NODE}")(rw,sync,no_subtree_check)" #>> /etc/exports
 
-#     echo "Allowing client through debian ip-tables : \t" $(docker node inspect --format "{{.Status.Addr}}" ${NODE})
+     echo "Allowing client through debian ip-tables : \t" $(docker node inspect --format "{{.Status.Addr}}" ${NODE})
 #     echo 'sudo -u iptables -A INPUT -s '$(docker node inspect --format "{{.Status.Addr}}" ${NODE})' -j ACCEPT'
-#     echo '\n'
-# done
+     echo '\n'
+ done
 
 # read -p "Do you want to install NFS server here ? (y/n) " RESP
 # if [ "$RESP" = "y" ]; then
@@ -72,6 +72,6 @@
 #     echo "Proceeding with the initialization"
 # fi
 
-echo 'please provide the NFS server ip :'
-    read input
-    echo $input
+#echo 'please provide the NFS server ip :'
+#    read input
+#    echo $input
