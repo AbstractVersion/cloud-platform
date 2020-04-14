14a1f216a1ee3270db5c3f27ad67a2a7747d52d0
George Fiotakis <g.fiotakis@hotmail.com>
2020-04-14 18:03:15 +0200

    Install docker script

---
+++ installation-scripts/install-docker-debian.sh
@@ -0,0 +1,31 @@
+# Uninstall old versions🔗
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker 
+
+docker run hello-world
