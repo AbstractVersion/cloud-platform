09f32f44ffcbc111d979927d7fea4f2079cb2a3b
George Fiotakis <g.fiotakis@hotmail.com>
2020-04-16 16:19:49 +0200

    modify to wait for eurika

--- hello-service/wait_from_cloud_config.sh
+++ hello-service/wait_from_cloud_config.sh
@@ -3,9 +3,9 @@
 
 
 
-bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' config-server:8888/actuator)" != "200" ]]; do sleep 5; done'
+bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' abstract:admin@discovery-service:8761/actuator)" != "200" ]]; do sleep 5; done'
 
 
->&2 echo "Cloud Config server is up & running - executing command"
+>&2 echo "Discovery server is up & running - executing command"
 
 exec "$@"
