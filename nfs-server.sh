0b3496a3e0b05656c3171e20c5b475a102eef0db
George Fiotakis <g.fiotakis@hotmail.com>
2020-04-16 15:43:34 +0200

    Update to v.1

--- nfs-server.sh
+++ nfs-server.sh
@@ -1,6 +1,7 @@
 #COnfiguration of server
 # In order to mount NFS drives we need to install the following:
 sudo -u root apt install nfs-common -y
+sudo -u root apt install  nfs-utils -y
 # https://sysadmins.co.za/docker-swarm-persistent-storage-with-nfs/
 # Create NFS Server:
 # We have 2 options, we can either install a NFS Server (you can follow this post to setup a NFS Server )  
@@ -12,9 +13,10 @@ mkdir /nfsdata
 ## Change the ip to the ip of the NFS server host
 docker run --rm -itd --name nfs \
   --privileged \
-  -v /nfsdata:/nfs.1 \
+  --network=host
+  -v /nfsdata/:/nfs.1 \
   -e SHARED_DIRECTORY=/nfs.1 \
-  -p 192.168.2.4:2049:2049 \
+  -p 192.168.2.5:2049:2049 \
   itsthenetwork/nfs-server-alpine:latest
 
 #   Configuration of Clients
@@ -23,10 +25,10 @@ sudo -u root apt install nfs-common -y
 # Install theese external tool for volume share among managers
 # Install Netshare Docker Volume Driver
 # Install Netshare which will provide the NFS Docker Volume Driver:
-$ wget https://github.com/ContainX/docker-volume-netshare/releases/download/v0.36/docker-volume-netshare_0.36_amd64.deb
-$ dpkg -i docker-volume-netshare_0.36_amd64.deb
+wget https://github.com/ContainX/docker-volume-netshare/releases/download/v0.36/docker-volume-netshare_0.36_amd64.deb
+dpkg -i docker-volume-netshare_0.36_amd64.deb
 $ service docker-volume-netshare start
-
+docker run -i -t --volume-driver=nfs --rm -v 192.168.2.4/foobar2:/mount ubuntu /bin/bash
 # docker create volume
 docker volume create --driver nfs --name foobar2 -o share=192.168.2.4:/foobar2
 docker volume inspect foobar2
