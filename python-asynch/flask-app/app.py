15897c4c8b5f1331ffe9e3eeb2f21e5070f05172
George Fiotakis <g.fiotakis@hotmail.com>
2020-08-24 23:50:22 +0200

    temporary export port

--- python-asynch/flask-app/app.py
+++ python-asynch/flask-app/app.py
@@ -8,6 +8,11 @@ import celery.states as states
 env=os.environ
 app = Flask(__name__)
 
+@app.route('/service-status')
+def add(param1,param2):
+    json = {"status": "up"}
+    return json
+
 @app.route('/add/<int:param1>/<int:param2>')
 def add(param1,param2):
     json = {"hi": "yes"}
