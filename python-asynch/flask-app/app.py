08797c32dcdabec1f114a95dd6245b3bd10565c5
George Fiotakis <g.fiotakis@hotmail.com>
2020-08-25 00:42:24 +0200

    add eurika client & json logging

--- python-asynch/flask-app/app.py
+++ python-asynch/flask-app/app.py
@@ -1,17 +1,51 @@
 import os
+import py_eureka_client.eureka_client as eureka_client
 from flask import Flask
-from flask import url_for, request
+from flask import  url_for, jsonify, request, make_response
 from worker import celery
 from celery.result import AsyncResult
 import celery.states as states
-import json
+import uuid , json
+import socket
+import sleuth
+import b3
+from elk_logger import logger_init
+import  logging, sys, json_logging
+from worker import celery
+
+
+os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
+
+
+serviceId = uuid.uuid1()
+serviceHost = socket.gethostname()
+
+# # init the logger as usual
+logger_init()
+logger = logging.getLogger("werkzeug")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
 env=os.environ
+app_name = "python-service"
+
+# Create some test data for our catalog in the form of a list of dictionaries.
+info = {'servicID': serviceId,
+     'serviceHost': serviceHost,
+     'title': 'Python API v.01'}
+
 app = Flask(__name__)
+CORS(app)
+
+#Configure Eurika client
+eureka_client.init(eureka_server="http://abstract:admin@discovery-service:8761/eureka",
+		   instance_port=5001,
+           app_name=app_name,
+		   ha_strategy=eureka_client.HA_STRATEGY_STICK)
 
 @app.route('/service-status')
 def status():
-    return "UP"
+    return jsonify(info)
 
 @app.route('/add/<int:param1>/<int:param2>')
 def add(param1,param2):
@@ -22,8 +56,11 @@ def add(param1,param2):
 
 @app.route('/longTask', methods=['POST'])
 def longTask():
+    b3.start_span()
     json = request.json
+    logger.info("Recieved project : " + json.dumps(jsonRequest))
     task = celery.send_task('mytasks.longtask', args=[json], kwargs={})
+    b3.end_span()
     return "<a href='{url}'>check status of {id} </a>".format(id=task.id,
                 url=url_for('check_task',id=task.id,_external=True))
 
@@ -37,6 +74,8 @@ def check_task(id):
     else:
         return str(res.result)
 if __name__ == '__main__':
+    app.before_request(b3.start_span)
+    app.after_request(b3.end_span)
     app.run(debug=env.get('DEBUG',True),
             port=int(env.get('PORT',5000)),
             host=env.get('HOST','0.0.0.0')
---
+++ python-asynch/flask-app/elk_logger.py
@@ -0,0 +1,87 @@
+# This example shows how the logger can be set up to use a custom JSON format.
+import logging
+import json
+import traceback
+from datetime import datetime
+import copy
+import json_logging
+import sys
+import sleuth
+import b3
+json_logging.ENABLE_JSON_LOGGING = True
+
+#logger_name = None
+#application_name = None
+version = '1'
+
+def extra(**kw):
+    '''Add the required nested props layer'''
+    return {'extra': {'props': kw}}
+
+
+class CustomJSONLog(logging.Formatter):
+    """
+    Customized logger
+    """
+    python_log_prefix = 'python.'
+    def get_exc_fields(self, record):
+        if record.exc_info:
+            exc_info = self.format_exception(record.exc_info)
+        else:
+            exc_info = record.exc_text
+        return {f'{self.python_log_prefix}exc_info': exc_info}
+
+    @classmethod
+    def format_exception(cls, exc_info):
+        return ''.join(traceback.format_exception(*exc_info)) if exc_info else ''
+
+    def format(self, record):
+        json_log_object = {"@timestamp": datetime.utcnow().isoformat(),
+                           "@version": version,
+                           "level": record.levelname,
+                           "message": record.getMessage(),
+                           "caller": record.filename + '::' + record.funcName,
+                           "logger_name": "python-logger",
+                           "application_name": "py-app"
+                           }
+        json_log_object['data'] = {
+            f'{self.python_log_prefix}logger_name': record.name,
+            f'{self.python_log_prefix}module': record.module,
+            f'{self.python_log_prefix}funcName': record.funcName,
+            f'{self.python_log_prefix}filename': record.filename,
+            f'{self.python_log_prefix}lineno': record.lineno,
+            f'{self.python_log_prefix}thread': f'{record.threadName}[{record.thread}]',
+            f'{self.python_log_prefix}pid': record.process
+        }
+        json_log_object ['trace'] ={
+            "trace_id":b3.values()['X-B3-TraceId'],
+            "span_id":b3.values()['X-B3-TraceId'],
+            "exportable":"false"
+        }
+        if hasattr(record, 'props'):
+            json_log_object['data'].update(record.props)
+
+        if record.exc_info or record.exc_text:
+            json_log_object['data'].update(self.get_exc_fields(record))
+
+        return json.dumps(json_log_object)
+
+
+def logger_init():
+    json_logging.__init(custom_formatter=CustomJSONLog)
+
+# You would normally import logger_init and setup the logger in your main module - e.g.
+# main.py
+
+
+
+# logger = logging.getLogger("werkzeug")
+# logger.setLevel(logging.DEBUG)
+# logger.addHandler(logging.StreamHandler(sys.stderr))
+
+# def halloLog():
+#     logger.info('Starting')
+#     try:
+#         1/0
+#     except: # noqa pylint: disable=bare-except
+#         logger.exception('You can\'t divide by zero')
