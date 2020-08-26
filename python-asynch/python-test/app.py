d4b25c7d42c6f40945185f382202d5ab364e9aa3
George Fiotakis <g.fiotakis@hotmail.com>
2020-08-26 19:16:11 +0200

    update test loggin pyhton

--- python-asynch/python-test/app.py
+++ python-asynch/python-test/app.py
@@ -8,12 +8,22 @@ import sleuth, b3
 
 app = flask.Flask(__name__)
 
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+    def add_fields(self, log_record, record, message_dict):
+        super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
+        if not log_record.get('timestamp'):
+            # this doesn't use record.created, so it is slightly off
+            now = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            log_record['timestamp'] = now
+        if log_record.get('level'):
+            log_record['level'] = log_record['level'].upper()
+        else:
+            log_record['level'] = record.levelname
+
 logger = logging.getLogger("test-logger")
 logger.setLevel(logging.DEBUG)
 json_handler = logging.StreamHandler()
-formatter = jsonlogger.JsonFormatter(
-        fmt='%(asctime)s %(levelname)s %(name)s %(message)s %(application_name)s'
-    )
+formatter = CustomJsonFormatter('(timestamp) (level) (name) (message)')
 json_handler.setFormatter(formatter)
 logger.addHandler(json_handler)
 
@@ -39,7 +49,7 @@ def api_all():
     logger.info("Yes Please")
     # logger.debug(b3.values()['X-B3-TraceId'], extra = {'props' : {'extra_property' : 'extra_value'}})
     # logger.info("Custom Logger message Python API", extra = buildTraceInfo(app_name,b3.values()['X-B3-TraceId']) )
-    logger.info("test log statement", application_name = "python-service" , extra = {'trace' : {
+    logger.info("test log statement", extra = {'trace' : {
             "trace_id":b3.values()['X-B3-TraceId'],
             "span_id":b3.values()['X-B3-TraceId'],
             "exportable":"false"
