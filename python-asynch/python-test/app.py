9a9bde7d20bb31fa6400cb2cd09156f31838258f
George Fiotakis <g.fiotakis@hotmail.com>
2020-08-26 19:19:27 +0200

    update test loggin pyhton

--- python-asynch/python-test/app.py
+++ python-asynch/python-test/app.py
@@ -13,7 +13,7 @@ class CustomJsonFormatter(jsonlogger.JsonFormatter):
         super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
         if not log_record.get('timestamp'):
             # this doesn't use record.created, so it is slightly off
-            now = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            now = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
             log_record['timestamp'] = now
         if log_record.get('level'):
             log_record['level'] = log_record['level'].upper()
