a9d05b503ddea9890707481fe684f29691084b6a
George Fiotakis <g.fiotakis@hotmail.com>
2020-09-20 23:36:10 +0200

    Tune high

--- eurika-group/eureka-server-peer1/src/main/java/com/eureka/server/demo/DemoApplication.java
+++ eurika-group/eureka-server-peer1/src/main/java/com/eureka/server/demo/DemoApplication.java
@@ -3,12 +3,12 @@ package com.eureka.server.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class DemoApplication  extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
