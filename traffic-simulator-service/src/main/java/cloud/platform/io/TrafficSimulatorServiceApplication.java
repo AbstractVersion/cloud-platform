49a091b12a03c52f4d9c15ee3282cc9a3f5bf3ae
George Fiotakis <g.fiotakis@hotmail.com>
2020-10-04 00:32:55 +0200

    Add v0.1 of traffic simulator

---
+++ traffic-simulator-service/src/main/java/cloud/platform/io/TrafficSimulatorServiceApplication.java
@@ -0,0 +1,13 @@
+package cloud.platform.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficSimulatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficSimulatorServiceApplication.class, args);
+	}
+
+}
---
+++ traffic-simulator-service/src/test/java/cloud/platform/io/TrafficSimulatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.platform.io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficSimulatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
